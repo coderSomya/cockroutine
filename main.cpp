e02acd47641c98d882bbfc9a58aabb2d3f826ee7
Somya <isomya13@gmail.com>
2026-05-23 18:30:52 +0530

    add some more code

--- main.cpp
+++ main.cpp
@@ -4,20 +4,42 @@
 
 struct Task{
     struct promise_type{
-        void return_void() {}
+        void return_value(int value) {returned_value = value;}
         auto initial_suspend(){ return std::suspend_never{};}
         auto final_suspend() noexcept { return std::suspend_always{};}
-        Task get_return_object(){ return Task();}
+        Task get_return_object(){ 
+            return Task{
+                std::coroutine_handle<promise_type>::from_promise(*this)
+            };
+        }
         void unhandled_exception(){ std::abort();}
+
+        int returned_value;
     };
+
+    std::coroutine_handle<promise_type> handle;
+
+    ~Task(){
+        std::cout<<"inside destructor of task"<<std::endl;
+        if(handle){
+            handle.destroy();
+        }
+    }
+
+
+    int get_returned_value(){
+        return handle.promise().returned_value;
+    }
 };
 
 Task f(){
-    co_return;
+    std::cout<<"inside f"<<std::endl;
+    co_return 42;
 }
 
 int main(){
-
-    f();
+    
+    auto task = f();
+    std::cout<<task.get_returned_value()<<std::endl;
     return 0;
 }
